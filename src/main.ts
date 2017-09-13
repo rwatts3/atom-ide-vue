a261b56d88adab3d6c25c570879ec0558ba3fbbc
Ryan Watts <ryandwatts@gmail.com>
2017-09-13 09:12:19 -0700

    bump deps house cleaning

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-const path = require('path');
-const {AutoLanguageClient} = require('atom-languageclient');
-const {filter} = require('fuzzaldrin-plus');
-const cp = require('child_process');
+import path from 'path';
+import { AutoLanguageClient } from 'atom-languageclient';
+import { filter } from 'fuzzaldrin-plus';
+import cp = require('child_process');
 
 class VueLanguageClient extends AutoLanguageClient {
 	private _serverManager;
@@ -12,57 +12,11 @@ class VueLanguageClient extends AutoLanguageClient {
 
 	startServerProcess () {
 		return cp.spawn('node', [require.resolve('vue-language-server/dist/vueServerMain')]);
-		// return super.spawnChildNode([ require.resolve('vue-language-server/dist/vueServerMain') ]);
 	};
 
 	preInitialization (connection) {
 		connection.onCustom('$/partialResult', () => {}); // Suppress partialResult until the language server honours 'streaming' detection
 	}
-
-	async getSuggestions (request) {
-		const prefix = request.prefix.trim();
-		const server = await this._serverManager.getServer(request.editor);
-
-		if (prefix === '' && !request.activatedManually) {
-			server.currentSuggestions = [];
-			return Promise.resolve([]);
-		}
-
-		if (prefix.length > 0 && prefix != '.'  && server.currentSuggestions && server.currentSuggestions.length > 0) {
-			// fuzzy filter on this.currentSuggestions
-			return new Promise((resolve) => {
-				const filtered = filter(server.currentSuggestions, prefix, {key: 'text'})
-					.map(s => Object.assign({}, s, {replacementPrefix: prefix}));
-				resolve(filtered);
-			});
-		}
-
-		if (request.activatedManually === true || request.prefix.startsWith('.')) {
-			return this.requestAndCleanSuggestions(request)
-				.then(suggestions => server.currentSuggestions = suggestions);
-			} else {
-				server.currentSuggestions = [];
-				return Promise.resolve([]);
-			}
-		}
-		
-
-	requestAndCleanSuggestions (request) {
-		return super.getSuggestions(request).then(results => {
-			if (results != null) {
-			for (const result of results) {
-				if (result.leftLabel) {
-					const index = result.leftLabel.lastIndexOf(':');
-						if (index > 0) {
-							result.leftLabel = result.leftLabel.substr(index + 1).trim();
-						}
-					}
-				}
-			}
-			
-			return results
-		});
-	}
 }
 
-module.exports = new VueLanguageClient();
+export =  new VueLanguageClient();
